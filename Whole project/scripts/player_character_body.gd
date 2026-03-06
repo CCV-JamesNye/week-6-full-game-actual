f3207cb4d9c9f331da2754bcb9d604a22ce0d9f2
xvrwllc <xvrwllc@gmail.com>
2026-03-05 20:24:41 -0500

    week 6 full game initial commit

--- Whole project/scripts/player_character_body.gd
+++ Whole project/scripts/player_character_body.gd
@@ -34,3 +34,4 @@ func _physics_process(delta: float) -> void:
 func _unhandled_input(event: InputEvent) -> void:
 	if event.is_action_pressed("jump") and is_on_floor():
 		velocity.y = jump_force
+fdasdf
